7d4b9b22108fc6dd935eef0b602dee8f26ebdc09
Pablo Moyano <pabmoy1725@gmail.com>
2023-11-09 15:30:51 -0300

    Modificación de sintaxis en ejercicios 4 y 6

--- js/6-perimetro.js
+++ js/6-perimetro.js
@@ -10,13 +10,11 @@ lado B = 5
 Output: 58
 */
 
-
-const ladoA = parseFloat(prompt('Ingrese el valor de Lado A'));
-const ladoB = parseFloat(prompt('Ingrese el valor de Lado B'));
-
 function perimetro(ladoA,ladoB){
  const formulaDePerimetro =  2 * (ladoA + ladoB);
  document.write(`<h2>El resultado del Perímetro es ${formulaDePerimetro}</h2>`)
 }
 
+const ladoA = parseFloat(prompt('Ingrese el valor de Lado A'));
+const ladoB = parseFloat(prompt('Ingrese el valor de Lado B'));
 perimetro(ladoA,ladoB);
